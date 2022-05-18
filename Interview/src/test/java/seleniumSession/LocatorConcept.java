5eda7e1f4e6f34a8c4c4d423377e34517c52860d
Gaurav Kumar Dabas <gauravkumardabas@gmail.com>
2022-05-19 01:35:21 +0530

    3rd Commit- Form complete - need to validate table - 19-05-22

--- Interview/src/test/java/seleniumSession/LocatorConcept.java
+++ Interview/src/test/java/seleniumSession/LocatorConcept.java
@@ -5,12 +5,13 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class LocatorConcept {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		System.setProperty("Webdriver.chrome.driver", "C:\\Java_Lib\\chromedriver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\Java_Lib\\chromedriver\\chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();
 
@@ -18,11 +19,30 @@ public class LocatorConcept {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
 
-		driver.get("https://www.ebay.com/");
+		driver.get("https://mytestingthoughts.com/Sample/home.html");
 
-		driver.findElement(By.linkText("register")).click();
 
-		Thread.sleep(1000);
+		driver.findElement(By.name("first_name")).sendKeys("Gaurav");
+		driver.findElement(By.name("last_name")).sendKeys("Dabas");
+		driver.findElement(By.name("inlineRadioOptions")).click();
+		Select Hobbies = new Select(driver.findElement(By.id("exampleFormControlSelect2")));
+		Hobbies.selectByIndex(0);
+		Hobbies.selectByIndex(1);
+		
+		Select office = new Select(driver.findElement(By.name("department")));
+		office.selectByVisibleText("MCR");
+		
+		driver.findElement(By.name("user_name")).sendKeys("Gauravkumardabas");
+		driver.findElement(By.name("user_password")).sendKeys("Alpha@12");
+		driver.findElement(By.name("confirm_password")).sendKeys("Alpha@12");
+		
+		driver.findElement(By.name("email")).sendKeys("test@test.com");
+		driver.findElement(By.name("contact_no")).sendKeys("(615)8265412");
+		driver.findElement(By.id("exampleFormControlTextarea1")).sendKeys("Enter long details to check if it works");
+		
+		driver.findElement(By.tagName("button")).submit();
+				
+		Thread.sleep(5000);
 		driver.quit();
 
 	}
