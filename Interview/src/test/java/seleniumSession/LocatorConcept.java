1dd8fbc31329b61e9536862d27a911f2bceb0de3
Gaurav Kumar Dabas <gauravkumardabas@gmail.com>
2022-05-19 00:50:44 +0530

    Staged 2nd commit - 19-05-22

--- Interview/src/test/java/seleniumSession/LocatorConcept.java
+++ Interview/src/test/java/seleniumSession/LocatorConcept.java
@@ -2,26 +2,29 @@ package seleniumSession;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LocatorConcept {
-	
-	public static void main(String[] args) {
-		
+
+	public static void main(String[] args) throws InterruptedException {
+
 		System.setProperty("Webdriver.chrome.driver", "C:\\Java_Lib\\chromedriver\\chromedriver.exe");
-		
+
 		WebDriver driver = new ChromeDriver();
-		
-		
+
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
-		
+
 		driver.get("https://www.ebay.com/");
-		
-		
-		
+
+		driver.findElement(By.linkText("register")).click();
+
+		Thread.sleep(1000);
+		driver.quit();
+
 	}
 
 }
